86d5f2eb22f0568a641c7588cbe94afece1fe9e8
Brian Quach <13rianquach@gmail.com>
2016-08-25 20:57:25 -0700

    fix: image dimensions

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,23 @@
 var gulp = require('gulp');
 var imageResize = require('gulp-image-resize');
 var rename = require('gulp-rename');
+var resizeMyPhotoTask = 'resizeMyPhotoTask';
 
 // Code inspired by http://stackoverflow.com/questions/35801807/gulp-image-resize-to-generate-multiple-output-sizes
 var resizeImageTasks = [];
 [300, 350].forEach(function(size) {
   var resizeImageTask = 'resize_' + size;
-
+  
   gulp.task(resizeImageTask, function() {
     return gulp.src('img_src/*.png')
       .pipe(imageResize({
-          imageMagick: true,
-          width: size
-        })
-      ).pipe(rename(function (path) {
+        imageMagick: true,
+        width: size
+      })).pipe(imageResize({
+        imageMagick: true,
+        crop: true,
+        height: size/2
+      })).pipe(rename(function (path) {
           path.basename += ("-" + size);
         }
       )).pipe(
@@ -24,5 +28,24 @@ var resizeImageTasks = [];
   });
   resizeImageTasks.push(resizeImageTask);
 });
+//
+// [300, 600].forEach(function(size) {
+//   resizeImageTask = 'resize_me_photo_' + size;
+//
+//   gulp.task(resizeImageTask, function() {
+//     return gulp.src('img_src/me.jpg')
+//       .pipe(imageResize({
+//           imageMagick: true,
+//           width: size
+//         })
+//       ).pipe(rename(function (path) {
+//           path.basename += ("-" + size);
+//         }
+//       )).pipe(
+//         gulp.dest('portfolio/img')
+//       );
+//   });
+//   resizeImageTasks.push(resizeImageTask);
+// });
 
 gulp.task('default', resizeImageTasks);

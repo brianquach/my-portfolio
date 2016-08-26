fc1df3dc2c7f2004a68cfa12a5f72d4c45c17fea
Brian Quach <13rianquach@gmail.com>
2016-08-25 20:24:11 -0700

    refact: gulp image task
    gulp resizes images for multiple widths more efficiently.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,28 @@
+// Copyright 2016 Brian Quach
+// Licensed under MIT (https://github.com/brianquach/udacity-nano-frontend-portfolio/blob/master/LICENSE)
 var gulp = require('gulp');
 var imageResize = require('gulp-image-resize');
 var rename = require('gulp-rename');
 
-gulp.task('imageOptimization', function () {
-  gulp.src('img_src/*.png')
-    .pipe(imageResize({
-        imageMagick: true,
-        width : 300,
-        height : 150
-      })
-    ).pipe(rename(function (path) {
-        path.basename += "-300w";
-      }
-    )).pipe(
-      gulp.dest('portfolio/img')
-    );
+// Code inspired by http://stackoverflow.com/questions/35801807/gulp-image-resize-to-generate-multiple-output-sizes
+var resizeImageTasks = [];
+[300, 350].forEach(function(size) {
+  var resizeImageTask = 'resize_' + size;
+
+  gulp.task(resizeImageTask, function() {
+    return gulp.src('img_src/*.png')
+      .pipe(imageResize({
+          imageMagick: true,
+          width: size
+        })
+      ).pipe(rename(function (path) {
+          path.basename += ("-" + size);
+        }
+      )).pipe(
+        gulp.dest('portfolio/img')
+      );
+  });
+  resizeImageTasks.push(resizeImageTask);
 });
 
-gulp.task('default', ['imageOptimization']);
+gulp.task('default', resizeImageTasks);
